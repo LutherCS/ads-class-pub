fef6b5aa69853c076d5838d15af2a68db60057f0
yasinovskyy <yasinovskyyr@uiu.edu>
2017-02-12 21:15:39 -0600

    The zoo exercise

---
+++ exercise_zoo/__init__.py
@@ -0,0 +1,8 @@
+'''Import all from Animals'''
+__all__ = ["Parrot", "Penguin", "Dog", "HouseCat", "BobCat"]
+
+from .Animals import Parrot
+from .Animals import Penguin
+from .Animals import Dog
+from .Animals import HouseCat
+from .Animals import BobCat

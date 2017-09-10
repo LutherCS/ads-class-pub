f015e52a83696e8cc9c81fe169505dbca10aeedc
Roman Yasinovskyy <yasiro01@luther.edu>
2017-09-10 16:52:54 -0500

    Started Bank project

---
+++ code_templates/project_bank/Bank.py
@@ -0,0 +1,33 @@
+'''
+Complete the code to simulate a bank
+'''
+#!/usr/bin/env python3
+
+import abc
+
+
+class Address:
+    pass
+
+
+class Customer:
+    pass
+
+
+class Account:
+    pass
+
+
+class CheckingAccount:
+    pass
+
+
+class SavingsAccount:
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ code_templates/project_bank/TestBank.py
@@ -0,0 +1,26 @@
+'''
+Testing the Bank project
+Roman Yasinovskyy, 2017
+'''
+
+#!/usr/bin/python3
+
+import unittest
+from unittest.mock import patch
+from io import StringIO
+
+
+class TestBankMethods(unittest.TestCase):
+    '''Testing module Bank'''
+
+    def setUp(self):
+        '''Setting up'''
+        pass
+
+    def test_address(self):
+        '''Testing making new address'''
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)

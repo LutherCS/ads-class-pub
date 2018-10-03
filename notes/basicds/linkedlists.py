7e13742b27c8679f0e5b0d9e8cb33834249b2e68
Roman Yasinovskyy <yasiro01@luther.edu>
2018-10-03 14:38:46 -0500

    Update Linked List notes

--- notes/basicds/linkedlists.py
+++ notes/basicds/linkedlists.py
@@ -25,8 +25,23 @@ class Node:
 class LinkedList:
     def __init__(self):
         self._head = None
-        self._tail = None
+        # self._tail = None
         self._size = 0
+    
+    def __len__(self) -> int:
+        return self._size
+    
+    def __str__(self):
+        list_str = '['
+        current = self._head
+
+        while current:
+            list_str += str(current)
+            if current.next:
+                list_str += ', '
+            current = current.next
+        list_str += ']'
+        return list_str
     
     def is_empty(self) -> bool:
         return self._head is None
@@ -35,42 +50,89 @@ class LinkedList:
         return self._size
     
     def add(self, new_node: object) -> None:
-        if not self._head:
-            self._head = new_node
-        else:
-            raise NotImplementedError
-    
-    def __str__(self):
-        my_list = ''
+        new_node.next = self._head
+        self._head = new_node
+        self._size += 1
+
+    def search(self, value) -> bool:
         current = self._head
         while current:
-            my_list += current.data
+            if current.data == value:
+                return True
             current = current.next
-        
-        return my_list
+        return False
+
+    def index(self, value) -> int:
+        idx = 0
+        current = self._head
+        while current:
+            if current.data == value:
+                return idx
+            current = current.next
+            idx += 1
+        return -1
+
+    def append(self, new_node):
+        pass
+
+    def insert(self, pos: int, new_node: object) -> None:
+        current = self._head
+        idx = 0
+        if pos <= 0:
+            self.add(new_node)
+            return
+        if pos > self._size:
+            self.append(new_node)
+            return
+        while idx < pos - 1:
+            idx += 1
+            current = current.next
+
+        new_node.next = current.next
+        current.next = new_node
+        self._size += 1
 
 
 def main():
-    print('Working with nodes')
-    n = Node('A')
-    print(n.data)
-    print(n.next)
-    print(n)
+    # print('Working with nodes')
+    # n = Node('A')
+    # print(n.data)
+    # print(n.next)
+    # print(n)
 
-    m = Node('B')
-    n.next = m
-    print(n.next)
+    # m = Node('B')
+    # n.next = m
+    # print(n.next)
 
     print('Working with lists')
     ll = LinkedList()
-    print(ll.size())
-    print(type(ll))
+    print(ll.size())  # 0
+    print(type(ll))  # LinkedList
     print('Printing a list')
     print(ll)
+    ll.add(Node('Q'))
+    print(ll)  # [Q]
     ll.add(Node('A'))
-    print(ll)
-
-
+    print(ll)  # [A, Q]
+    ll.add(Node('D'))
+    print(ll)  # [D, A, Q]
+    print(len(ll))
+    print(ll.search('Z'))
+    print(ll)  # [D, A, Q]
+    print(len(ll))
+    print(ll.index('D'))  # 0
+    print(ll)  # [D, A, Q]
+    print(len(ll))
+    print(ll.index('Z'))  # -1
+    print(ll)  # [D, A, Q]
+    print(len(ll))
+    print('Inserting a new node')
+    ll.insert(0, Node('K'))
+    print(ll)  # [K, D, A, Q]
+    print(len(ll))  # 4
+    ll.insert(2, Node('M'))
+    print(ll)  # [K, D, M, A, Q]
+    print(len(ll))  # 5
 
 if __name__ == '__main__':
     main()

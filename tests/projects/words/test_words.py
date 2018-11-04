aa104bcd444d172c5a567f449095057b1c2c36e8
Roman Yasinovskyy <yasiro01@luther.edu>
2018-11-04 09:58:55 -0600

    Add Word Ladder project

---
+++ tests/projects/words/test_words.py
@@ -0,0 +1,52 @@
+'''
+Testing the module words
+Karina Hoff, 2018
+'''
+
+#!/usr/bin/python3
+
+"""Testing the Word Ladder project"""
+import pytest
+from projects.words.words import Stack, Queue, read_file, diff_by_one, diff_by_one_all
+
+
+class TestWordsMethods:
+    '''Testing module words'''
+
+    @pytest.fixture(scope='function', autouse=True)
+    def setup_class(self):
+        '''Setting up'''
+        pass
+        
+    def test_read_file(self):
+        '''Test read_file functino'''
+        assert read_file("data/projects/words/words.txt") == {3: 1294, 4: 4994, 5: 5757}
+    
+    def test_diff_by_one(self):
+        '''Test diff_by_one function'''
+        w1 = 'Hello'
+        w2 = 'Hello'
+        assert diff_by_one(w1, w2) == 0
+        w1 = 'Hello'
+        w2 = 'Jello'
+        assert diff_by_one(w1, w2) == 1
+        w1 = 'Hello'
+        w2 = 'Happy'
+        assert diff_by_one(w1, w2) == 4
+    
+    def test_diff_by_one_all(self):
+        '''Testing diff_by_one_all function'''
+        #no used words
+        word = 'Hello'
+        all_words = ['Jello', 'Happy']
+        used_words = []
+        assert diff_by_one_all(word, all_words, used_words) == ['Jello']
+        #used words
+        word = 'Hello'
+        all_words = ['Jello', 'Happy']
+        used_words = ['Jello']
+        assert diff_by_one_all(word, all_words, used_words) == []
+        
+
+if __name__ == '__main__':
+    pytest.main(['test_words.py'])

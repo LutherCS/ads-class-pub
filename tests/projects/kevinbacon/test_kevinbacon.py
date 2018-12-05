7e168c69f6a42f1dbf22686dc3ac848ad1fd837e
Roman Yasinovskyy <yasiro01@luther.edu>
2018-12-05 14:57:01 -0600

    Update Kevin Bacon project test

--- tests/projects/kevinbacon/test_kevinbacon.py
+++ tests/projects/kevinbacon/test_kevinbacon.py
@@ -1,62 +1,37 @@
-'''
+"""
 Testing the module movies
 Karina Hoff, 2018
-'''
+"""
 
 #!/usr/bin/python3
 
+
 import pytest
-from projects.kevinbacon.kevinbacon import read_file, find_max_kbn_actors, main
+from src.projects.kevinbacon import read_file
 
 
 class TestMoviesMethods:
-    '''Testing module movies'''
+    """Testing module movies"""
 
-    @classmethod
-    def setup_class(cls):
-        '''Setting up'''
-        cls.graph = read_file(sys.path[0] + "/projects/moviestat/movie_actors_small.txt")
-        kevin = cls.graph.get_vertex("Kevin Bacon")
-        cls.graph.bfs(kevin)        
-        
+    def setup_class(self):
+        """Setting up"""
+        self.graph = read_file("data/projects/kevinbacon/movie_actors_test.txt")
+        kevin = self.graph.get_vertex("Kevin Bacon")
+        self.graph.bfs(kevin)
+
+
     def test_read_file(self):
-        assert len(self.graph) == 216251
+        assert len(self.graph) == 7
         connections = 0
         for v in self.graph:
             connections += len(v.get_neighbors())
-        assert connections == 3162842
-        
-    def test_find_max_kbn_actors(self):
-        assert self.graph != None
-        expected_lst = ["Jason Wyssman",
-                        "Joe Maccarthey",
-                        "Jordan (V) Hale",
-                        "Robert G. Heron",
-                        "Jordin Billington",
-                        "Tom (IV) O'Rourke",
-                        "Philip Davies",
-                        "Darren Shields" ]
-        lst = find_max_kbn_actors(self.graph)
-        for actor in expected_lst:
-            assert actor in lst
+        assert connections == 18
 
-    def test_main(self, capsys, monkeypatch):
-        # check functionality of input
-        expected_output1 = "1) Julie Andrews acted with Colin Firth in Relative Values (2000)\n" + \
-                        "2) Colin Firth acted with Kevin Bacon in Where the Truth Lies (2005)\n" + \
-                        "\n" + \
-                        "Julie Andrews's Kevin Bacon number is 2\n"
-        # path can vary; just check number here
-        expected_output2 = "Chris Bannow's Kevin Bacon number is 4\n"
-        # try one or more inputs
-        inputs = ['Julie Andrews', 'Chris Bannow', '']
-        input_gen = (actor for actor in inputs)
-        monkeypatch.setattr('builtins.input', lambda prompt: next(input_gen))
-        main()
-        out, err = capsys.readouterr()
-        assert expected_output1 in out
-        assert expected_output2 in out
+    def test_bfs(self):
+        assert self.graph.get_vertex("Allen").get_distance() == 1
+        assert self.graph.get_vertex("Brad").get_distance() == 2
+        assert self.graph.get_vertex("Grace").get_distance() == 3
 
 
-if __name__ == '__main__':
-    pytest.main(['test_kbmovies.py'])
+if __name__ == "__main__":
+    pytest.main(["test_kevinbacon.py"])

8e63b0fc63b1cb1a01ff84a5e1c09aef2f6e154a
Roman Yasinovskyy <yasiro01@luther.edu>
2018-12-08 18:24:48 -0600

    Update Kevin Bacon tests

--- tests/projects/kevinbacon/test_kevinbacon.py
+++ tests/projects/kevinbacon/test_kevinbacon.py
@@ -7,7 +7,7 @@ Karina Hoff, 2018
 
 
 import pytest
-from src.projects.kevinbacon import read_file
+from projects.kevinbacon import read_file
 
 
 class TestMoviesMethods:
@@ -20,18 +20,28 @@ class TestMoviesMethods:
         self.graph.bfs(kevin)
 
 
-    def test_read_file(self):
+    def test_read_file_vertices(self):
         assert len(self.graph) == 7
+    
+    def test_read_file_edges(self):
         connections = 0
         for v in self.graph:
-            connections += len(v.get_neighbors())
+            connections += len(v.get_all_neighbors())
+            # connections += len(v.get_neighbors())
         assert connections == 18
 
     def test_bfs(self):
         assert self.graph.get_vertex("Allen").get_distance() == 1
-        assert self.graph.get_vertex("Brad").get_distance() == 2
-        assert self.graph.get_vertex("Grace").get_distance() == 3
+        assert self.graph.get_vertex("Tom").get_distance() == 2
+        assert self.graph.get_vertex("David").get_distance() == 3
 
+    def test_traversal(self):
+        road_to_kb = []
+        v = self.graph.get_vertex("David")
+        while v:
+            road_to_kb.append(v.get_key())
+            v = v.get_previous()
+        assert " ".join(road_to_kb) == "David Tom Jennifer Kevin Bacon"
 
 if __name__ == "__main__":
     pytest.main(["test_kevinbacon.py"])

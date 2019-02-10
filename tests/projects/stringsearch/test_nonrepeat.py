1b9638562bf375a63b596d14d00e60ed9068b436
Roman Yasinovskyy <yasinovskyy@gmail.com>
2019-02-10 17:12:36 -0600

    Update String search tests

--- tests/projects/stringsearch/test_nonrepeat.py
+++ tests/projects/stringsearch/test_nonrepeat.py
@@ -15,7 +15,7 @@ TIME_LIMIT = 4
 @pytest.mark.timeout(TIME_LIMIT)
 @pytest.mark.parametrize(
     "a_string, an_answer",
-    [("abxaby", "x"), ("y", "y"), ("abxbac", "x"), (ascii_lowercase, "a")],
+    [("abxaby", "x"), ("y", "y"), ("abxxbac", "x"), (ascii_lowercase, "a")],
 )
 def test_find_non_repeat(a_string, an_answer):
     """Testing simple cases"""
@@ -25,7 +25,7 @@ def test_find_non_repeat(a_string, an_answer):
 
 @pytest.mark.timeout(TIME_LIMIT)
 @pytest.mark.parametrize("str_len", [10, 100, 10000, 1_000_000])
-def test_find_non_repeat_long_unique(str_len):
+def test_find_non_repeat_long_beginning(str_len):
     """Testing long string with a non-repeating symbol at the beginning"""
     a_string = "".join([chr(n) for n in range(1, str_len)])
     result = find_non_repeat(a_string)
@@ -34,7 +34,7 @@ def test_find_non_repeat_long_unique(str_len):
 
 @pytest.mark.timeout(TIME_LIMIT)
 @pytest.mark.parametrize("str_len", [10, 100, 10000, 1_000_000])
-def test_find_non_repeat_long_success(str_len):
+def test_find_non_repeat_long_end(str_len):
     """Testing long string with a non-repeating symbol at the end"""
     assert find_non_repeat("z" * str_len + "a") == "a"
 
@@ -50,7 +50,7 @@ def test_find_non_repeat_error(a_string):
 
 
 @pytest.mark.timeout(TIME_LIMIT)
-def test_find_non_repeat_long_error():
+def test_find_non_repeat_error_long():
     """Testing a long string without a non-repeating symbol"""
     with pytest.raises(ValueError) as excinfo:
         find_non_repeat("z" * 1_000_000)

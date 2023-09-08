02368f1b0f611fc41eae40e5006a79999e071cbc
Roman Yasinovskyy <yasinovskyy@gmail.com>
2023-09-08 10:19:11 -0500

    Add various class implementations

--- notes/Introduction/oop.py
+++ notes/Introduction/oop.py
@@ -46,3 +46,163 @@ class Car:
 
 car8 = Car(5, "pink", "2023")
 print(f"Our {car8.color} car was made in {car8.year} and has {car8.wheels} wheels")
+car8.year = 1923
+print(f"Our {car8.color} car was made in {car8.year} and has {car8.wheels} wheels")
+
+
+class Car:
+    def __init__(self) -> None:
+        self.wheels = 4
+        self.year = 1923
+        self.color = "black"
+
+
+car9 = Car()
+print(f"Our {car9.color} car was made in {car9.year} and has {car9.wheels} wheels")
+car9.year = 2023
+print(f"Our {car9.color} car was made in {car9.year} and has {car9.wheels} wheels")
+
+
+class Car:
+    def __init__(
+        self, wheels_param: int = 4, year_param: int = 1923, color_param: str = "black"
+    ) -> None:
+        self.wheels = wheels_param
+        self.year = year_param
+        self.color = color_param
+
+
+car10 = Car()
+print(f"Our {car10.color} car was made in {car10.year} and has {car10.wheels} wheels")
+car11 = Car(1, 2023, "rainbow")
+print(f"Our {car11.color} car was made in {car11.year} and has {car11.wheels} wheels")
+car12 = Car(2, 2020)
+print(f"Our {car12.color} car was made in {car12.year} and has {car12.wheels} wheels")
+car13 = Car(3, "white")
+print(f"Our {car13.color} car was made in {car13.year} and has {car13.wheels} wheels")
+car14 = Car(wheels_param=3, color_param="white")
+print(f"Our {car14.color} car was made in {car14.year} and has {car14.wheels} wheels")
+
+
+class Car:
+    def __init__(self, wheels_param: int, year_param: int, color_param: str) -> None:
+        self.wheels = wheels_param
+        self.year = year_param
+        self.color = color_param
+
+
+# car15 = Car()
+# print(f"Our {car15.color} car was made in {car15.year} and has {car15.wheels} wheels")
+
+
+class Car:
+    def __init__(
+        self, wheels_param: int = 4, year_param: int = 1923, color_param: str = "black"
+    ) -> None:
+        self._wheels = wheels_param
+        self._year = year_param
+        self._color = color_param
+
+    def get_wheels(self) -> int:
+        return self._wheels
+
+    def get_year(self) -> int:
+        return self._year
+
+    def get_color(self) -> str:
+        return self._color
+
+
+car15 = Car(wheels_param=3, color_param="white")
+print(
+    f"Our {car15._color} car was made in {car15._year} and has {car15._wheels} wheels"
+)
+car15._year = 2023
+print(
+    f"Our {car15._color} car was made in {car15._year} and has {car15._wheels} wheels"
+)
+print(
+    f"Our {car15.get_color()} car was made in {car15.get_year()} and has {car15.get_wheels()} wheels"
+)
+
+
+class Car:
+    def __init__(
+        self, wheels_param: int = 4, year_param: int = 1923, color_param: str = "black"
+    ) -> None:
+        self._wheels = wheels_param
+        self._year = year_param
+        self._color = color_param
+
+    def get_wheels(self) -> int:
+        return self._wheels
+
+    def get_year(self) -> int:
+        return self._year
+
+    def get_color(self) -> str:
+        return self._color
+
+    def set_wheels(self, new_value: int) -> None:
+        self._wheels = new_value
+
+    def set_color(self, new_value: str) -> None:
+        self._color = new_value
+
+    wheels = property(get_wheels, set_wheels)
+    color = property(get_color, set_color)
+    year = property(get_year)
+
+
+car16 = Car()
+print(
+    f"Our {car16.get_color()} car was made in {car16.get_year()} and has {car16.get_wheels()} wheels"
+)
+car16.set_color("green")
+print(
+    f"Our {car16.get_color()} car was made in {car16.get_year()} and has {car16.get_wheels()} wheels"
+)
+
+car17 = Car()
+print(f"Our {car17.color} car was made in {car17.year} and has {car17.wheels} wheels")
+# car17.year= 2023
+# print(f"Our {car17.color} car was made in {car17.year} and has {car17.wheels} wheels")
+car17._year = 2023
+print(f"Our {car17.color} car was made in {car17.year} and has {car17.wheels} wheels")
+
+
+class Car:
+    def __init__(
+        self, wheels_param: int = 4, year_param: int = 1923, color_param: str = "black"
+    ) -> None:
+        self._wheels = wheels_param
+        self._year = year_param
+        self._color = color_param
+
+    def get_wheels(self) -> int:
+        return self._wheels
+
+    def get_year(self) -> int:
+        return self._year
+
+    def get_color(self) -> str:
+        return self._color
+
+    def set_wheels(self, new_value: int) -> None:
+        self._wheels = new_value
+
+    def set_color(self, new_value: str) -> None:
+        self._color = new_value
+
+    wheels = property(get_wheels, set_wheels)
+    color = property(get_color, set_color)
+    year = property(get_year)
+
+    def paint(self, new_color):
+        self._color = new_color
+
+
+car20 = Car()
+print(f"Our {car20.color} car was made in {car20.year} and has {car20.wheels} wheels")
+car20.paint("green")
+print(f"Our {car20.color} car was made in {car20.year} and has {car20.wheels} wheels")
